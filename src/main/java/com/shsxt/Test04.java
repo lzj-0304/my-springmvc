2f10788357aed8d6f6d9ad8441e917ed9ce36d01
joy <lzj_0304@163.com>
2019-12-12 10:59:20 +0800

    dev_2.0--->test04 文件更新

--- src/main/java/com/shsxt/Test04.java
+++ src/main/java/com/shsxt/Test04.java
@@ -3,5 +3,6 @@ package com.shsxt;
 public class Test04 {
     public static void main(String[] args) {
         System.out.println("test04....");
+        System.out.println("dev_2.0--->test04....");
     }
 }

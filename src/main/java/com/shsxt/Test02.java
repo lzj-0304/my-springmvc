330f14c5f39034a92ce2c9294f66341fc4c4a230
joy <lzj_0304@163.com>
2019-12-12 09:21:04 +0800

    添加test02 测试文件

--- src/main/java/com/shsxt/Test02.java
+++ src/main/java/com/shsxt/Test02.java
@@ -4,5 +4,7 @@ public class Test02 {
 
     public static void main(String[] args) {
         System.out.println("test02...");
+        System.out.println("test02...");
+        System.out.println("test02...");
     }
 }
